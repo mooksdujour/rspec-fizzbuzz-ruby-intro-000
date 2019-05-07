20f15e079304ae4307a007d7f22c125186a78b5f
John Moo-King <jmooking@hotmail.com>
2019-05-07 21:16:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(n)
-  if n % 3 == 0 && n % 5 != 0 
+  if n % 3 == 0 && n % 5 != 0
     return "Fizz"
   elsif n % 5 == 0 && n % 3 != 0
     return "Buzz"
@@ -11,4 +11,3 @@ def fizzbuzz(n)
     return nilend
   end
 end
-    
